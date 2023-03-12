b64b79cac51e9cd1e24c0e5f1e6bfac5117ea21f
YvesJaques <ymoraisjaques@gmail.com>
2023-03-11 21:40:20 -0300

    If & Formatting

--- gotut2.go
+++ gotut2.go
@@ -46,4 +46,27 @@ func main() {
 	// cV9 := fmt.Sprintf("%.2f", 3.14)
 	// pl(cV9)
 
+	// If Conditional
+	// Conditional Operators : > < >= <= == !=
+	// Logical Operators : && || !
+
+	// iAge := 8
+	// if (iAge >= 1) && (iAge <= 18) {
+	// 	pl("Important Birthday")
+	// } else if (iAge == 21) || (iAge == 50) {
+	// 	pl("Important Birthday")
+	// } else if iAge >= 65 {
+	// 	pl("Important Birthday")
+	// } else {
+	// 	pl("Not an Important Birthday")
+	// }
+
+	// pl("!true", !true)
+
+	// fmt.Printf("%s %d %c %f %t %o %x\n", "Stuff", 1, 'A', 3.14, true, 1, 1)
+	// fmt.Printf("%9f\n", 3.14)
+	// fmt.Printf("%.2f\n", 3.141592)
+	// fmt.Printf("%9.f\n", 3.141592)
+	// sp1 := fmt.Sprintf("%9.f\n", 3.141592)
+	// pl(sp1)
 }
