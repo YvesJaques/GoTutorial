9ff044900a9067e825d17e30fc9fa52caa6a80aa
YvesJaques <ymoraisjaques@gmail.com>
2023-03-11 21:37:59 -0300

    Variables & Data Types

---
+++ gotut2.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+var pl = fmt.Println
+
+func main() {
+	// var vName string = "Yves"
+	// var v1, v2 = 1.2, 3.4
+	// var v3 = "Hello"
+	// v1 = 4.5
+
+	// int, float64, bool, string, rune
+	// Default type 0, 0.0, false, ""
+
+	// pl(reflect.TypeOf(25))
+	// pl(reflect.TypeOf(3.14))
+	// pl(reflect.TypeOf(true))
+	// pl(reflect.TypeOf("Hello"))
+	// pl(reflect.TypeOf('🦍'))
+
+	// cV1 := 1.5
+	// cV2 := int(cV1)
+	// pl(cV2)
+
+	// cV3 := "5000000"
+	// cV4, err := strconv.Atoi(cV3)
+	// pl(cV4, err, reflect.TypeOf(cV4))
+
+	// cV5 := 50000000
+	// cV6 := strconv.Itoa(cV5)
+	// pl(cV6)
+
+	// cV7 := "3.14"
+	// if cV8, err := strconv.ParseFloat(cV7, 64); err ==
+	// 	nil {
+	// 	pl(cV8)
+	// }
+
+	// cV9 := fmt.Sprintf("%.2f", 3.14)
+	// pl(cV9)
+
+}
