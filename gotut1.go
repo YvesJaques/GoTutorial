a90da200ea9780d4255c6f15c3073ffaadc71dcd
YvesJaques <ymoraisjaques@gmail.com>
2023-03-11 21:35:29 -0300

    Introduction

---
+++ gotut1.go
@@ -0,0 +1,22 @@
+package main
+
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	f "fmt"
+// 	"log"
+// 	"os"
+// )
+
+// var pl = f.Println
+
+// func main() {
+// 	fmt.Print("What is your name? ")
+// 	reader := bufio.NewReader(os.Stdin)
+// 	name, err := reader.ReadString('\n')
+// 	if err == nil {
+// 		pl("Hello", name)
+// 	} else {
+// 		log.Fatal(err)
+// 	}
+// }
