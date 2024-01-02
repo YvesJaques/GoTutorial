bc84556afc697ec8fc13b740dbe450ecb0e3004d
YvesJaques <ymoraisjaques@gmail.com>
2024-01-02 15:32:04 -0300

    Slices

--- gotut2.go
+++ gotut2.go
@@ -294,4 +294,38 @@ func main() {
 	// byteArr := []byte{'a', 'b', 'c'}
 	// bStr := string(byteArr[:])
 	// pl("I'm a string:", bStr)
+
+	// // Slices
+	// sl1 := make([]string, 6)
+	// sl1[0] = "Society"
+	// sl1[1] = "of"
+	// sl1[2] = "the"
+	// sl1[3] = "Simulated"
+	// sl1[4] = "Universe"
+	// pl("Slice Size:", len(sl1))
+	// for i := 0; i < len(sl1); i++ {
+	// 	pl(sl1[i])
+	// }
+	// for _, x := range sl1 {
+	// 	pl(x)
+	// }
+	// sl2 := []int{12, 21, 1974}
+	// pl(sl2)
+
+	// sArr := [5]int{1, 2, 3, 4, 5}
+	// sl3 := sArr[0:2]
+	// pl("First 2:", sl3)
+
+	// sArr[0] = 10
+	// pl("sl3:", sl3)
+
+	// sl3 = append(sl3, 12)
+	// pl("sl3:", sl3)
+	// pl("sArr:", sArr)
+
+	// // nil values
+	// sl4 := make([]string, 6)
+	// pl("sl4 :", sl4)
+	// pl("sl4[0] :", sl4[0])
+
 }
