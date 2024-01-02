67c6c47e7727a096fbde6867f96e30f095e0612f
YvesJaques <ymoraisjaques@gmail.com>
2024-01-02 14:33:08 -0300

    Date & Time

--- gotut2.go
+++ gotut2.go
@@ -241,4 +241,28 @@ func main() {
 	// 	fmt.Printf("%d : %#U : %c\n", i, runeVal, runeVal)
 	// }
 
+	// // Date & Time
+	// now := time.Now()
+	// pl(now.Year(), now.Month(), now.Day())
+	// pl(now.Hour(), now.Minute(), now.Second())
+	// loc, err := time.LoadLocation("America/New_York")
+	// if err != nil {
+	// 	pl(err)
+	// }
+	// fmt.Printf("Time in New York %s\n", now.In(loc))
+	// loc, _ = time.LoadLocation("Asia/Shanghai")
+	// fmt.Printf("Time in Shanghai %s\n", now.In(loc))
+
+	// locEST, _ := time.LoadLocation("EST")
+	// locUTC, _ := time.LoadLocation("UTC")
+	// locMST, _ := time.LoadLocation("MST")
+	// fmt.Printf("EST: %s\n", now.In(locEST))
+	// fmt.Printf("UTC: %s\n", now.In(locUTC))
+	// fmt.Printf("MST: %s\n", now.In(locMST))
+
+	// birthDate := time.Date(1974, time.December, 21, 11, 30, 10, 0, time.Local)
+	// diff := now.Sub(birthDate)
+	// fmt.Printf("Days Alive: %d days\n", int(diff.Hours()/24))
+	// fmt.Printf("Hours Alive: %d hours\n", int(diff.Hours()))
+
 }
