94447a5f8353ffb7d549a2368cd59d48680ef37b
YvesJaques <ymoraisjaques@gmail.com>
2024-01-04 14:36:41 -0300

    Interfaces

---
+++ interfaces.go
@@ -0,0 +1,37 @@
+package main
+
+// import "fmt"
+
+// var pl = fmt.Println
+
+// type Animal interface {
+// 	AngrySound()
+// 	HappySound()
+// }
+// type Cat string
+
+// func (c Cat) Attack() {
+// 	pl("Cat attacks its prey")
+// }
+
+// func (c Cat) Name() string {
+// 	return string(c)
+// }
+
+// func (c Cat) AngrySound() {
+// 	pl("Cat says Hissssss")
+// }
+
+// func (c Cat) HappySound() {
+// 	pl("Cat says Purrrr")
+// }
+
+// func main() {
+// 	var kitty Animal
+// 	kitty = Cat("Kitty")
+// 	kitty.AngrySound()
+
+// 	var kitty2 Cat = kitty.(Cat)
+// 	kitty2.Attack()
+// 	pl("Cat Name:", kitty2.Name())
+// }
