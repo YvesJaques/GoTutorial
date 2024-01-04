8059652b9aca5362abad798b79ac74677f4b38d8
YvesJaques <ymoraisjaques@gmail.com>
2024-01-04 15:27:45 -0300

    closures

---
+++ closures.go
@@ -0,0 +1,34 @@
+package main
+
+// import (
+// 	"fmt"
+// )
+
+// var pl = fmt.Println
+
+// func useFunc(f func(int, int) int, x, y int) {
+// 	pl("Answer:", (f(x, y)))
+// }
+
+// func sumVals(x, y int) int {
+// 	return x + y
+// }
+
+// func main() {
+// 	// function as a return value
+// 	intSum := func(x, y int) int {
+// 		return x + y
+// 	}
+// 	pl("5 + 4 =", intSum(5, 4))
+
+// 	//changing value
+// 	samp1 := 1
+// 	changeVar := func() {
+// 		samp1 += 1
+// 	}
+// 	changeVar()
+// 	pl("samp1 =", samp1)
+
+// 	// function as a parameter
+// 	useFunc(sumVals, 5, 8)
+// }
