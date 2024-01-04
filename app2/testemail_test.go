3e0c540c365f8de207902e0cad32b29fa10e4350
YvesJaques <ymoraisjaques@gmail.com>
2024-01-04 16:57:17 -0300

    Automated testing

---
+++ app2/testemail_test.go
@@ -0,0 +1,22 @@
+package app2
+
+// import (
+// 	"testing"
+// )
+
+// func TestIsEmail(t *testing.T) {
+// 	_, err := IsEmail("hello")
+// 	if err == nil {
+// 		t.Error("hello is not an email")
+// 	}
+
+// 	_, err = IsEmail("yves@aol.com")
+// 	if err != nil {
+// 		t.Error("yves@aol.com is an email")
+// 	}
+
+// 	_, err = IsEmail("yves@aol")
+// 	if err == nil {
+// 		t.Error("yves@aol is not an email")
+// 	}
+// }
