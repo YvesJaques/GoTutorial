ee89154c254f0d217ca73304bad1164b21cd75d2
YvesJaques <ymoraisjaques@gmail.com>
2024-01-04 16:10:13 -0300

    Recursion

---
+++ recursion.go
@@ -0,0 +1,18 @@
+package main
+
+// import (
+// 	"fmt"
+// )
+
+// var pl = fmt.Println
+
+// func factorial(num uint64) uint64 {
+// 	if num == 0 {
+// 		return 1
+// 	}
+// 	return num * factorial(num-1)
+// }
+
+// func main() {
+// 	pl("Factorial of 4 =", factorial(4))
+// }
