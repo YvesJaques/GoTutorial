59edacc1cbe31519f730f9a34a7ba223c1f2f470
YvesJaques <ymoraisjaques@gmail.com>
2024-01-04 10:18:46 -0300

    Maps

---
+++ maps.go
@@ -0,0 +1,28 @@
+package main
+
+// import "fmt"
+
+// var pl = fmt.Println
+
+// func main() {
+// 	// var myMap map[keyType]valueType
+// 	heroes := make(map[string]string)
+// 	villains := make(map[string]string)
+
+// 	heroes["Batman"] = "Bruce Wayne"
+// 	heroes["Superman"] = "Clark Kent"
+// 	heroes["The Flash"] = "Barry Allen"
+// 	villains["Lex Luthor"] = "Lex Luthor"
+
+// 	superPets := map[int]string{1: "Krypto", 2: "Bat Hound"}
+// 	fmt.Printf("Batman is %v\n", heroes["Batman"])
+
+// 	pl("Chip:", superPets[3])
+
+// 	_, ok := superPets[3]
+// 	pl("Is there a 3rd pet:", ok)
+
+// 	for k, v := range heroes {
+// 		fmt.Printf("%s is %s\n", k, v)
+// 	}
+// }
