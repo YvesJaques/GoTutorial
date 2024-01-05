7567ca585c5ea094e955145af856cc232e088a75
YvesJaques <ymoraisjaques@gmail.com>
2024-01-05 15:53:40 -0300

    Sudoku solver(not solving 100%)

--- sudoku.go
+++ sudoku.go
@@ -16,11 +16,19 @@ SUDOKU RULES
 // Outputs the current board to screen
 func displayBoard(puzz [][]int) {
 	puzz_cols := len(puzz[0])
+	fmt.Println("-------------------------")
 	for i := 0; i < len(puzz); i++ {
+		fmt.Print("| ")
 		for j := 0; j < puzz_cols; j++ {
 			fmt.Print(strconv.Itoa(puzz[i][j]) + " ")
+			if (j+1)%3 == 0 {
+				fmt.Print("| ")
+			}
 		}
 		fmt.Println()
+		if (i+1)%3 == 0 {
+			fmt.Println("-------------------------")
+		}
 	}
 }
 
@@ -76,6 +84,67 @@ func isNumValid(puzz [][]int, guess int, row int, column int) bool {
 	return true
 }
 
+/*
+-------------------------
+| 0 0 0 | 0 3 5 | 0 7 0 |
+| 2 5 0 | 0 4 6 | 8 0 1 |
+| 0 1 3 | 7 0 8 | 0 4 9 |
+-------------------------
+| 1 9 0 | 0 0 7 | 0 0 4 |
+| 0 0 5 | 0 0 2 | 0 9 6 |
+| 8 0 2 | 0 9 4 | 0 0 7 |
+-------------------------
+| 3 7 0 | 0 0 9 | 0 0 0 |
+| 0 6 1 | 0 7 0 | 0 0 0 |
+| 4 0 0 | 5 8 1 | 0 0 0 |
+-------------------------
+
+### actual solution
+-------------------------
+| 9 8 4 | 1 3 5 | 6 7 2 |
+| 2 5 7 | 9 4 6 | 8 3 1 |
+| 6 1 3 | 7 2 8 | 5 4 9 |
+-------------------------
+| 1 9 6 | 3 5 7 | 2 8 4 |
+| 7 4 5 | 8 1 2 | 3 9 6 |
+| 8 3 2 | 6 9 4 | 1 5 7 |
+-------------------------
+| 3 7 8 | 2 6 9 | 4 1 5 |
+| 5 6 1 | 4 7 3 | 9 2 8 |
+| 4 2 9 | 5 8 1 | 7 6 3 |
+-------------------------
+
+
+// Recursion Problem (Solution)
+// 1. Cycle across the rows column by column (1-9)
+// 2. Check if valid number
+// a. If true update array
+// b. If false change back to 0
+// c. If false find a new value for previous we
+// thought was correct
+// 3. Check next column
+*/
+
+func solvePuzzle(puzz [][]int) bool {
+	row, column := getEmptySpace(puzz)
+	if row == -1 {
+		return true
+	} else {
+		row, column = getEmptySpace(puzz)
+	}
+	for i := 1; i <= 9; i++ {
+		if isNumValid(puzz, i, row, column) {
+			puzz[row][column] = i
+
+			if solvePuzzle(puzz) {
+				return true
+			}
+			puzz[row][column] = 0
+		}
+	}
+	return false
+}
+
 func main() {
 	puzz := [][]int{
 		{0, 0, 0, 0, 3, 5, 0, 7, 0},
@@ -90,19 +159,17 @@ func main() {
 	}
 	displayBoard(puzz)
 
-	row, _ := getEmptySpace(puzz)
-	if row != -1 {
-		fmt.Println(getEmptySpace(puzz))
-	} else {
-		fmt.Println("Solved")
-		return
-	}
+	// // Return first open space
+	// row, _ := getEmptySpace(puzz)
+	// if row != -1 {
+	// 	fmt.Println(getEmptySpace(puzz))
+	// } else {
+	// 	fmt.Println("Solved")
+	// 	return
+	// }
 
-	fmt.Println(isNumValid(puzz, 1, 0, 0))
-	fmt.Println(isNumValid(puzz, 7, 0, 0))
-	fmt.Println(isNumValid(puzz, 6, 0, 0))
-	fmt.Println(isNumValid(puzz, 9, 0, 0))
+	solvePuzzle(puzz)
+	fmt.Println("##########################")
+	displayBoard(puzz)
 
-	fmt.Println(isNumValid(puzz, 7, 4, 0))
-	fmt.Println(isNumValid(puzz, 1, 0, 6))
 }
